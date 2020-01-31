38324ffc28bbb10938a25431ce7c5580b7fa63fb
antrondallis <32026646+antrondallis@users.noreply.github.com>
2020-01-31 09:50:25 -0500

    End of Section 11

--- DatingApp-Web/src/app/members/member-edit/member-edit.component.ts
+++ DatingApp-Web/src/app/members/member-edit/member-edit.component.ts
@@ -14,6 +14,7 @@ import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 })
 export class MemberEditComponent implements OnInit {
   user: User;
+  photoUrl: string;
   @ViewChild('editForm', {static: true}) editForm: NgForm;
   @HostListener('window:beforeunload', ['$event'])
   unloadNotification($event: any) {
@@ -28,6 +29,7 @@ export class MemberEditComponent implements OnInit {
     this.route.data.subscribe(data => {
       this.user = data['user'];
     });
+    this.authService.currentPhotoUrl.subscribe(photoUrl => this.photoUrl = photoUrl)
   }
 
   updateUser() {
@@ -39,5 +41,9 @@ export class MemberEditComponent implements OnInit {
         this.alertify.error(error);
       }
     );
+  }
+
+  updateMainPhoto(photoUrl) {
+    this.user.photoUrl = photoUrl;
   }
 }
